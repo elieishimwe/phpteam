5b8a907db8cac802703ae8650c6097bf19b0b615
Thabiso Ngobese <thabisotngobese@gmail.com>
2017-02-22 09:10:05 +0200

    added reverse exercice

--- reverse/logRMain.php
+++ reverse/logRMain.php
@@ -1,10 +1,10 @@
 
 <?php
 
-
+//pre defined
 $data="Thabiso Ngobese";
 
-  /*function myFunR($data)
+  function myFunR($data)
   {
       echo strrev($data);
 
@@ -12,44 +12,4 @@ $data="Thabiso Ngobese";
 
 
   echo myFunR($data);
-
-*/
-
-
-//$count=5;
-
-/*$x=6;
-$count=10;
-while($x<$count)
-{
-
-    echo $x;
-    $x++;
-    $count--;
-}
-/*
-for($i=0; $i>0; $i++)
-{
-
-    echo ($i-$i);
-}
-echo "Tamjs";
-$data="Thabiso";
-$n=10;
-$nn =sizeof($data);
-for($i=$n;  $i>=0 ; $i--)
-{
-    echo $i;
-}
-*/
-
-
-$text="Hello World and Coders";
-$length = strlen($text);
-
-for ($i = $length; $i > 0; $i--)
-{
-    echo $text[$i-1];
-}
-
 ?>
